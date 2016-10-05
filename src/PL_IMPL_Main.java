588ce5729ea72507020decae8c0f5b3949ce38ab
Mattia Villani <mattiavillani94@gmail.com>
2016-10-05 16:34:40 +0200

    no package

--- src/PL_IMPL_Main.java
+++ src/PL_IMPL_Main.java
@@ -1,10 +1,15 @@
+
 /**
  * Created by matti on 05/10/2016.
  */
 public class PL_IMPL_Main {
 
     public static void main(String[] argv){
-        System.out.println("Hi");
+        Scanner scanner = new Scanner("C:\\Users\\matti\\Desktop\\Prova.txt");
+        Parser parser = new Parser(scanner);
+        parser.Parse();
+        Token token = parser.la;
+        System.out.println(token.val);
     }
 
 }
